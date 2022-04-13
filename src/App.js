d8e184d6b06cfa65f14d5c7180bdf20868f4ec43
Ania Wawerek <wawerekanna@gmail.com>
2022-04-13 18:38:07 +0200

    12.1 Add Timer

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Button from './components/Button/Button';
+import React, {useEffect, useState} from "react";
+import Timer from './components/Timer/Timer';
+import Container from "./components/Container/Container";
+
+
+
+const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if(start) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval)
+  }, [start]);
+
+  return (
+    <Container>
+      <Timer time={time} />
+    
+      <Button action={() => setStart(true)}>Start</Button>
+      <Button action={() => setStart(false)}>Stop</Button>
+      <Button action={() => {setTime(0); setStart(false);}}>Reset</Button>
+    </Container>
+        
+     
+  );
+
+}
+
+export default App;
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,15 @@
+import styles from './Timer.module.scss';
+
+
+const Timer = ({ time }) => {
+  
+        return(
+            <div className={styles.time}>
+              <span>{("0" + Math.floor(time / 3600000) % 24).slice(-2)}:</span>
+              <span>{("0" + Math.floor(time / 60000) % 60).slice(-2)}:</span>
+              <span>{("0" + Math.floor(time / 1000) % 60).slice(-2)}.</span>
+              <span>{("" + (time / 10) % 100).slice(-2)}</span>
+            </div>
+        )};
+
+export default Timer;
